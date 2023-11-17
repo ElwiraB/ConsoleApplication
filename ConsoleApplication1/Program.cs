5315adcd6c9466d78227d26c9302b1c3f367add7
EBRO <ebro@simcorp.com>
2023-11-17 11:31:56 +0100

    add new line

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("Hello world");
             Console.WriteLine("My name is Elwira");
+            Console.WriteLine("Hi!");
         }
     }
 }
